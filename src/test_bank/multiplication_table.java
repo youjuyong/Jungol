74b7f8ca003d6b93492914625d98e5b2a2eaf7d0
YouJuYong <dbwndyd518@naver.com>
2022-06-04 20:59:53 +0900

    2022/06/04 수정

--- src/test_bank/multiplication_table.java
+++ src/test_bank/multiplication_table.java
@@ -14,20 +14,51 @@ public class multiplication_table {
 		start_num = sc.nextInt();
 		final_num = sc.nextInt();
 		
-		for(int j=start_num;j<=final_num;j++) {
-		
+		if(start_num<final_num) {
+			
+			for(int j=start_num;j<=final_num;j++) {
+
+				for(int i=1;i<10;i++) {
+					if(i%3==0) {
+						System.out.printf("%d * %d = %2d  " ,j,i,(j*i));
+						System.out.println();
+					}else{
+						System.out.printf("%d * %d = %2d  " ,j,i,(j*i));
+						System.out.printf(" ");
+					}
+				}
+
+				System.out.println();
+			}
+		}else if(start_num>final_num) {
+			
+			for(int j=start_num;j>=final_num;j--) {
+
+				for(int i=1;i<10;i++) {
+					if(i%3==0) {
+						System.out.printf("%d * %d = %2d  " ,j,i,(j*i));
+						System.out.println();
+					}else{
+						System.out.printf("%d * %d = %2d  " ,j,i,(j*i));
+						System.out.printf(" ");
+					}
+				}
+
+				System.out.println();
+			}
+		}else {
 			for(int i=1;i<10;i++) {
 				if(i%3==0) {
-					System.out.printf("%d * %d = %2d" ,j,i,(j*i));
+					System.out.printf("%d * %d = %2d  " ,start_num,i,(start_num*i));
 					System.out.println();
 				}else{
-					System.out.printf("%d * %d = %2d" ,j,i,(j*i));
+					System.out.printf("%d * %d = %2d  " ,start_num,i,(start_num*i));
 					System.out.printf(" ");
 				}
 			}
-			
-			System.out.println();
 		}
+	
+	
 	}
 
 }
